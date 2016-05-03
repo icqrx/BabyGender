733c4f82fea6033462c13af8e9f6b280deb99805
MarkNguyen <huuquoc09@gmail.com>
2016-05-03 16:53:01 -0500

    + List of Range Dates Function + Highlight range of dates on the calendar - remove data sample

--- app/src/main/java/com/marknguyen/babygenderpredictor/TimePredictorActivity.java
+++ app/src/main/java/com/marknguyen/babygenderpredictor/TimePredictorActivity.java
@@ -14,11 +14,15 @@ import android.widget.Toast;
 
 import com.squareup.timessquare.CalendarPickerView;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import datepicker.DatePicker;
 import materialdesign.views.ButtonRectangle;
@@ -45,40 +49,59 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_predictor);
-//        tv_solarBirthday = (TextView)findViewById(R.id.tv_solar_birthday2);
-        pickerSolarBirthday = (DatePicker)findViewById(R.id.long_date);
-        tv_lunarAge = (TextView)findViewById(R.id.tv_lunar_birthday2);
-        spRangeOfAge = (Spinner)findViewById(R.id.spinner_rangeOfAges);
-        tvTimeResults = (TextView)findViewById(R.id.tv_time_results);
-        swBoyorGirl = (Switch)findViewById(R.id.sw_boyorgirl);
-
-
-        final Calendar nextYear = Calendar.getInstance();
-        nextYear.add(Calendar.YEAR, 1);
+        pickerSolarBirthday = (DatePicker) findViewById(R.id.long_date);
+        tv_lunarAge = (TextView) findViewById(R.id.tv_lunar_birthday2);
+        spRangeOfAge = (Spinner) findViewById(R.id.spinner_rangeOfAges);
+        tvTimeResults = (TextView) findViewById(R.id.tv_time_results);
+        swBoyorGirl = (Switch) findViewById(R.id.sw_boyorgirl);
 
         final Calendar lastYear = Calendar.getInstance();
         lastYear.add(Calendar.YEAR, -1);
         final Date today = new Date();
 
-        int year = 2016;// generate a year between 1900 and 2010;
-        int dayOfYear = 4;// generate a number between 1 and 365 (or 366 if you need to handle leap year);
-                Calendar calendar = Calendar.getInstance();
 
-        thisYear =  calendar.get(Calendar.YEAR);
+        Calendar calendar = Calendar.getInstance();
+        thisYear = calendar.get(Calendar.YEAR);
 
-        final List<Date> dates = new ArrayList<>();
-        for (int i = 6; i < 10; i++){
-        calendar.set(Calendar.YEAR, 2016);
-        calendar.set(Calendar.DAY_OF_MONTH, i);
-        final Date randomDoB = calendar.getTime();
-            dates.add(randomDoB);
-        }
-        calendar.set(Calendar.YEAR, 1985);
-        calendar.set(Calendar.DAY_OF_MONTH, 15);
-        dates.add(calendar.getTime());
+//        //Mark Sample Data
+//        int year = 2016;// generate a year between 1900 and 2010;
+//        int dayOfYear = 4;// generate a number between 1 and 365 (or 366 if you need to handle leap year);
+//        final List<Date> dates = new ArrayList<>();
+//        final List<Date> dates1 = new ArrayList<>();
+//        for (int i = 6; i < 10; i++) {
+//            calendar.set(Calendar.YEAR, 2016);
+//            calendar.set(Calendar.DAY_OF_MONTH, i);
+//            final Date randomDoB = calendar.getTime();
+//            dates.add(randomDoB);
+//        }
+//        // 0 Jan, 1 Feb
+//        calendar.set(Calendar.YEAR, 2016);
+//        calendar.set(Calendar.MONTH, 4);
+//        calendar.set(Calendar.DAY_OF_MONTH, 15);
+//        dates1.add(calendar.getTime());
+//
+//        calendar.set(Calendar.YEAR, 2016);
+//        calendar.set(Calendar.MONTH, 4);
+//        calendar.set(Calendar.DAY_OF_MONTH, 19);
+//        dates1.add(calendar.getTime());
+//
+//        calendar.set(Calendar.YEAR, 2016);
+//        calendar.set(Calendar.MONTH, 4);
+//        calendar.set(Calendar.DAY_OF_MONTH, 23);
+//        dates1.add(calendar.getTime());
+//
+//        calendar.set(Calendar.YEAR, 2016);
+//        calendar.set(Calendar.MONTH, 5);
+//        calendar.set(Calendar.DAY_OF_MONTH, 19);
+//        dates1.add(calendar.getTime());
+//
+//        final Map<Date, Date> myMap = new HashMap<Date, Date>();
+//        myMap.put(dates1.get(0), dates1.get(1));
+//        myMap.put(dates1.get(2), dates1.get(3));
+
 // input zo range bao nhieu nam, input list ngày cần highlight)
 
-        btn_timePredictor = (ButtonRectangle)findViewById(R.id.btn_time_predictor);
+        btn_timePredictor = (ButtonRectangle) findViewById(R.id.btn_time_predictor);
         btn_timePredictor.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,14 +111,19 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
                     Boolean isCheck = swBoyorGirl.isChecked();
                     if (isCheck) temp = 1;
                     else temp = 0;
-                    getMonthXXX(temp);
+                    List<Map<Date,Date>> result  = getMonthXXX(temp);
+
                     String title = "Prediction time";
-                    //showCalendarInDialog(title, R.layout.dialog_calendarsquare);
-                    //dialogView.init(today, nextYear.getTime())
-                    //        .inMode(CalendarPickerView.SelectionMode.RANGE)
-                     //       .withSelectedDate(new Date())
-                     //       .withHighlightedDates(dates)
-                    //;
+                    showCalendarInDialog(title, R.layout.dialog_calendarsquare);
+                    final Calendar nextYear = Calendar.getInstance();
+                    nextYear.add(Calendar.YEAR, Integer.parseInt(spRangeOfAge.getSelectedItem().toString()));
+                    dialogView.init(today, nextYear.getTime())
+                            .inMode(CalendarPickerView.SelectionMode.SINGLE)
+                                    //.withSelectedDates(dates1)
+                                    //.withHighlightedDates(dates)
+                            .withHighlightedRangeDates(result);
+                    ;
+                    // set mode RANGE, input select 2 ngay
                 } catch (Exception e) {
                     Toast.makeText(TimePredictorActivity.this, "Please check the input again!", Toast.LENGTH_SHORT).show();
                 }
@@ -127,6 +155,7 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
 
     /**
      * THis fucntion to show the calendar square Mark
+     *
      * @param title
      * @param layoutResId
      */
@@ -136,35 +165,35 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
                 .setTitle(title)
                 .setView(dialogView)
                 .setNeutralButton("Dismiss", new DialogInterface.OnClickListener() {
-                    @Override public void onClick(DialogInterface dialogInterface, int i) {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
                         dialogInterface.dismiss();
                     }
                 })
                 .create();
         theDialog.setOnShowListener(new DialogInterface.OnShowListener() {
-            @Override public void onShow(DialogInterface dialogInterface) {
+            @Override
+            public void onShow(DialogInterface dialogInterface) {
                 dialogView.fixDialogDimens();
             }
         });
         theDialog.show();
     }
 
-    public int getLastDate(int month, int year)
-    {
+    public int getLastDate(int month, int year) {
         Calendar dateCal = Calendar.getInstance();
         dateCal.set(year, month, 2);
-        //String pattern = "MMMM";
-
-        //SimpleDateFormat obDateFormat = new SimpleDateFormat(pattern);
-        //String monthName = obDateFormat.format(dateCal.getTime());
         int maxDay = dateCal.getActualMaximum(Calendar.DAY_OF_MONTH);
         //return "Last date of " + monthName + " :" + maxDay;
-
         return maxDay;
     }
 
-    public String convertLunarMonthToSolarMonthRange(int luMonth, int luYear){
+    public Map<Date, Date> convertLunarMonthToSolarMonthRange(int luMonth, int luYear) {
+        Map<Date, Date> range = new HashMap<Date, Date>();
+        java.text.DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+        Date firstDay = null, lastDay = null;
         String solarMonthRange = "";
+
         Lunar luDate = new Lunar();
         Solar soDate = new Solar();
 
@@ -174,43 +203,69 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
         //Convert first date of a month
         luDate.lunarDay = 1;
         soDate = LunarSolarConverter.LunarToSolar(luDate);
-        solarMonthRange += soDate.solarYear + "/" + soDate.solarMonth +"/" + soDate.solarDay + " --> ";
+        solarMonthRange = soDate.solarMonth + "/" + soDate.solarDay + "/" + soDate.solarYear;
+        try {
+            firstDay = df.parse(solarMonthRange);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
         //Convert last date of a month
         //Find last date of a month
         luDate.lunarDay = getLastDate(luMonth, luYear);
         soDate = LunarSolarConverter.LunarToSolar(luDate);
-        solarMonthRange +=  soDate.solarYear + "/" + soDate.solarMonth + "/" + soDate.solarDay + "\n";
+        solarMonthRange = soDate.solarMonth + "/" + soDate.solarDay + "/" + soDate.solarYear;
+        try {
+            lastDay = df.parse(solarMonthRange);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 
-        return solarMonthRange;
+        range.put(firstDay, lastDay);
+        return range;
     }
 
-    public void getMonthXXX(int temp){
+    public List<Map<Date,Date>> getMonthXXX(int temp) {
+        List<Map<Date,Date>> listOfRange = new ArrayList<>();
+        Map<Date, Date> range = new HashMap<Date, Date>();
         String rangeAges = spRangeOfAge.getSelectedItem().toString();
         int tmp_age = ageMom;
-        int tmp = 0;
+        int tmp_year = 0;
         String tmp_month = "";
         String tmp_solar_month = "";
 
-        for(int i = 1; i <= 12; i++){
-            if (BoyorGirl(tmp_age,i) == temp){
+        for (int i = 1; i <= 12; i++) {
+            if (BoyorGirl(tmp_age, i) == temp) {
                 tmp_month += ", " + i;
-                tmp_solar_month += convertLunarMonthToSolarMonthRange(i, thisYear);
+                range =  convertLunarMonthToSolarMonthRange(i, thisYear + tmp_year);
+                listOfRange.add(range);
+
+                // show range of dates by textview
+                Iterator<Map.Entry<Date, Date>> iterator = range.entrySet().iterator();
+                Map.Entry<Date, Date> pairs = (Map.Entry<Date, Date>) iterator.next();
+                Date start = pairs.getKey();
+                Date end = pairs.getValue();
+                tmp_solar_month += start.toString() + "-->" + end.toString() + "\n";
             }
             if (i == 12) {
-                tvTimeResults.append("At the ages of " + tmp_age  + " the baby prediction time in months: -> " + tmp_month.substring(1, tmp_month.length()));
+                tvTimeResults.append("At the ages of " + tmp_age + " the baby prediction time in months: -> " + tmp_month.substring(1, tmp_month.length()));
                 tvTimeResults.append("\n");
-                tmp_age ++; i = 0; tmp ++; tmp_month = "";
+                tmp_age++;
+                i = 0;
+                tmp_year++;
+                tmp_month = "";
             }
-            if (tmp == Integer.parseInt(rangeAges)) {
+            if (tmp_year == Integer.parseInt(rangeAges)) {
                 break;
             }
         }
         tvTimeResults.append(tmp_solar_month);
+        return listOfRange;
     }
-    public int BoyorGirl(int ageMom, int lunarPregnatMonth){
 
-        return PickDayActivity.magic_table[ageMom-18][lunarPregnatMonth-1];
+    public int BoyorGirl(int ageMom, int lunarPregnatMonth) {
+
+        return PickDayActivity.magic_table[ageMom - 18][lunarPregnatMonth - 1];
     }
 
     @Override
@@ -225,6 +280,6 @@ public class TimePredictorActivity extends FragmentActivity implements DatePicke
     @Override
     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-        }
+    }
 
 }
